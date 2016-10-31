3f10b30f8813bb3e1d1d4612df594170d87a0606
Davor <davor.cesljas@fer.hr>
2016-10-31 21:48:21 +0100

    Extract redundancy and get ready for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
+  #svi ovi parametri dakle @article ili @articles se šalju u pripadne .html.erb datoteke prilikom poziva ovih funkcija
+  #jer ih upravo one i pozivaju
   def index
     @articles = Article.all
 
@@ -9,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -26,7 +29,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     # moramo zapisati parametre
     if @article.update(article_params)
       flash[:notice] = "Article was sucessfully edited"
@@ -37,16 +39,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully destroyed"
     redirect_to articles_path
   end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
